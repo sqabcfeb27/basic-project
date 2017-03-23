a74a465c86ff5feb64484cd653796abb44ef40b0
Jean-francois Nepton <sqasolution@hotmail.com>
2017-03-22 21:34:34 -0700

    Create DataHelper to Read From External Data Sources

---
+++ src/main/java/com/sqa/jf/helpers/enums/DataType.java
@@ -0,0 +1,23 @@
+/**
+ * File Name: DataType.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Mar 22, 2017
+ */
+package com.sqa.jf.helpers.enums;
+
+/**
+ * DataType //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public enum DataType {
+	STRING, INT, FLOAT, DOUBLE, CHAR, BOOLEAN, LONG, SHORT
+}
---
+++ src/main/java/com/sqa/jf/helpers/exceptions/CharacterCountFormatException.java
@@ -0,0 +1,5 @@
+package com.sqa.jf.helpers.exceptions;
+
+
+public class CharacterCountFormatException extends Exception {
+}
